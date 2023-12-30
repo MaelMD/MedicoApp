61508dad4f3b773c9d33427d52148be0c13d663a
md <md@md.com>
2023-12-30 15:46:44 +0100

    commit

--- MedicoApp/config-service/src/main/java/com/md/configservice/ConfigServiceApplication.java
+++ MedicoApp/config-service/src/main/java/com/md/configservice/ConfigServiceApplication.java
@@ -1,9 +1,12 @@
 package com.md.configservice;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
