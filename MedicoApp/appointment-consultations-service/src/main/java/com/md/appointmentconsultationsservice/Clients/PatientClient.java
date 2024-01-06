f5799ec7b9eda616a8f6c4d19fc4cc5121568e14
Yasser Ettoumi <ettoumiyassir@gmail.com>
2024-01-06 17:54:10 +0100

    Adding Circuit Breaker Resilience 4j

--- MedicoApp/appointment-consultations-service/src/main/java/com/md/appointmentconsultationsservice/Clients/PatientClient.java
+++ MedicoApp/appointment-consultations-service/src/main/java/com/md/appointmentconsultationsservice/Clients/PatientClient.java
@@ -1,6 +1,8 @@
 package com.md.appointmentconsultationsservice.Clients;
 
+import com.md.appointmentconsultationsservice.Model.Doctor;
 import com.md.appointmentconsultationsservice.Model.Patient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "PATIENTS-DOCTORS-SERVICE")
 public interface PatientClient {
     @GetMapping("/patients/find/{id}")
+    @CircuitBreaker(name = "patientService", fallbackMethod = "getDefaultPatient")
     Patient getPatientById(@PathVariable("id") Long id);
+    default Patient getDefaultPatient(Long id, Exception exception) {
+        Patient patient = new Patient();
+        patient.setId(id);
+        patient.setNom("not available");
+        patient.setEmail("not available");
+        return patient;
+    }
 }
